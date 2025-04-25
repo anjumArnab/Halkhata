ee2412e3f70d7b7dd6f456c62ce7699204021490
anjumArnab <anjumarnab050@gmail.com>
2025-04-25 20:08:38 +0600

    imported transaction_record.dart

--- lib/models/loan_record.dart
+++ lib/models/loan_record.dart
@@ -1,28 +1,20 @@
 // loan_record.dart
+import 'package:halkhata/models/transaction_record.dart';
+
 class LoanRecord {
-  final String id;
+  final int? id;
   final String name;
   final double amount;
   final DateTime date;
   double remainingAmount;
-  final List<Transaction> transactions;
+  List<Transaction> transactions;
 
-  LoanRecord({
-    this.id = '',
-    required this.name,
-    required this.amount,
-    required this.date,
-    required this.remainingAmount,
-    List<Transaction>? transactions,
-  }) : transactions = transactions ?? [];
-}
-
-class Transaction {
-  final double amount;
-  final DateTime date;
-
-  Transaction({
-    required this.amount,
-    required this.date,
-  });
+ LoanRecord({
+  this.id,
+  required this.name,
+  required this.amount,
+  required this.date,
+  required this.remainingAmount,
+   List<Transaction>? transactions
+}) : this.transactions = transactions ?? [];
 }
