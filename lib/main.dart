7560b8568a49f1300465fa5301566ddcb6dc9be5
anjumArnab <anjumarnab050@gmail.com>
2025-04-25 20:27:08 +0600

    hive init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:halkhata/models/loan_record.dart';
+import 'package:halkhata/models/transaction_record.dart';
 import 'package:halkhata/screens/login_page.dart';
+import 'package:hive_flutter/adapters.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Hive
+  await Hive.initFlutter();
+  
+  // Register adapters
+  Hive.registerAdapter(LoanRecordAdapter());
+  Hive.registerAdapter(TransactionAdapter());
+  
   runApp(const Halkhata());
 }
 
